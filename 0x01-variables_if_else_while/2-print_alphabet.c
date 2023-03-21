c9c76a73688ed39d45795411b01e59c6b34501e7
Emekablaise <ikenwaikee@gmail.com>
2023-03-20 17:24:01 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - program to print alphabets in lowercase using putchar
+ * Return: 0
+ */
+
+int main(void)
+{
+	int alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	for (alphabets = 'A'; alphabets <= 'Z'; alphabets++)
+	{
+		putchar(alphabets);
+	}
+	putchar('\n');
+	return (0);
+}
